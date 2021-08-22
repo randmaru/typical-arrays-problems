bd8fabfb5d453539f824e06269cdf1830c9adcb0
randmaru <git.randmaru@ya.ru>
2021-08-22 16:55:22 +0300

    fix: del spread

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 exports.min = min = (array) => {
-    array.length === 0 ? 0 : Math.min(...array);
+    array.length === 0 ? 0 : Math.min(array);
 };
 
 exports.max = max = (array) => {
-    array.length === 0 ? 0 : Math.max(...array);
+    array.length === 0 ? 0 : Math.max(array);
 };
 
 exports.avg = avg = (array) => {
-    array.length === 0 ? 0 : (Math.max(...array) + Math.min(...array)) / 2;
+    array.length === 0 ? 0 : (Math.max(array) + Math.min(array)) / 2;
 };
