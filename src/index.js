5c9e60dac0d5321583b2c7bc9258ffd531eb6ebd
randmaru <git.randmaru@ya.ru>
2021-08-22 18:48:00 +0300

    fix: delete else

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 exports.min = min = (...array) => {
     if (array.length == 0 || !array) {
         return 0;
-    } else {
-        return Math.min(...array);
     }
+    return Math.min(...array);
 };
 
 exports.max = max = (...array) => {
     if (array.length == 0 || !array) {
         return 0;
-    } else {
-        return Math.max(...array);
     }
+    return Math.max(...array);
 };
 
 exports.avg = avg = (...array) => {
     if (array.length == 0 || !array) {
         return 0;
-    } else {
-        return [...array].reduce((a, b) => a + b, 0) / array.length;
     }
+    return [...array].reduce((a, b) => a + b, 0) / array.length;
 };
 
 console.log(min(1, 2, 3, 4));
