9679325c849b3b770df5f6d5b8ad7a4dda6ed758
randmaru <git.randmaru@ya.ru>
2021-08-22 17:29:33 +0300

    refactor: fix ternar operators

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 exports.min = min = (array) => {
-    array.length === 0 ? 0 : Math.min(array);
+    if (array.length == 0) {
+        return 0;
+    } else {
+        return Math.min(array);
+    }
 };
 
 exports.max = max = (array) => {
-    array.length === 0 ? 0 : Math.max(array);
+    if (array.length == 0) {
+        return 0;
+    } else {
+        return Math.max(array);
+    }
 };
 
-exports.avg = avg = (array) => {
-    array.length === 0 ? 0 : array.reduce((a, b) => a + b) / array.length;
+exports.avg = avg = (...array) => {
+    if (array.length == 0) {
+        return 0;
+    } else {
+        return [...array].reduce((a, b) => a + b, 0) / array.length;
+    }
 };
+
+console.log(min(1, 2, 3, 4)); // returns 1;
+console.log(max(1, 2, 3, 4)); // returns 4;
+console.log(avg(1, 2, 3, 4)); // returns 2.5;
